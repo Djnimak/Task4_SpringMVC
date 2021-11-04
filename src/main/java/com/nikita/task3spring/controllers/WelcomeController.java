646397f9b921a3e9155a3d42ee29ce08b364119e
Nikita Makovskyi <djnimak@gmail.com>
2021-11-05 01:58:35 +0200

    Refactored the codeV2

--- src/main/java/com/nikita/task3spring/controllers/WelcomeController.java
+++ src/main/java/com/nikita/task3spring/controllers/WelcomeController.java
@@ -2,8 +2,8 @@ package com.nikita.task3spring.controllers;
 
 import com.nikita.task3spring.constants.Constants;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 
@@ -11,12 +11,12 @@ import org.springframework.web.servlet.ModelAndView;
 public class WelcomeController {
 
 
-    @RequestMapping("/")
+    @RequestMapping({"/", "", "/index"})
     public String welcome() {
         return "index";
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping(value = "/")
     public ModelAndView welcomePage() {
         return new ModelAndView(Constants.INDEX_JSP);
     }
